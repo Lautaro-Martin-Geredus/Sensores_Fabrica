42bef9bf1f12e40d3e71cd38920facf90ade23bf
Lautaro-Martin-Geredus <lautarogh789@gmail.com>
2024-08-19 15:22:02 -0300

    commit final

--- Backend/Api_Sensors/Api_Sensors/obj/Debug/net8.0/Api_Sensors.AssemblyInfo.cs
+++ Backend/Api_Sensors/Api_Sensors/obj/Debug/net8.0/Api_Sensors.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api_Sensors")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+907a31f41e9b484ff1faab9d6a833ade697faea3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b6f7ee20f89c2b2c38df2480058e4b7da78e74a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api_Sensors")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api_Sensors")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
