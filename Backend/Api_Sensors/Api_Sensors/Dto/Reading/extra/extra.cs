55d478a98fb7b83898abd472d93f8fff82c95ab3
Lautaro-Martin-Geredus <lautarogh789@gmail.com>
2024-09-17 16:32:05 -0300

    ajustando 1

---
+++ Backend/Api_Sensors/Api_Sensors/Dto/Reading/extra/extra.cs
@@ -0,0 +1,76 @@
+﻿namespace Api_Sensors.Dto.Reading.extra
+{
+    public class extra
+    {
+        /*
+         * using Api_Sensors.Dto.Reading;
+using Api_Sensors.Models;
+using Api_Sensors.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api_Sensors.Repository.Impl
+{
+    public class ReadingRepositoryImpl : IReadingRepository
+    {
+        private readonly ApiSensoresContext _context;
+        private readonly ISensorService _sensorService;
+
+        public ReadingRepositoryImpl(ApiSensoresContext context, ISensorService sensorService)
+        {
+            _context = context;
+            _sensorService = sensorService;
+        }
+
+        public async Task<List<ReadingDto>> GetReadingsByDates(string sensorName, DateOnly startDate, DateOnly endDate)
+        {
+            var readings = await _context.Readings
+                .Include(r => r.Sensor)
+                .Where(x => x.Sensor.Name == sensorName && x.ReadingDate >= startDate && x.ReadingDate <= endDate)
+                .ToListAsync();
+
+            var readingDto = readings.Select(x => new ReadingDto
+            {
+                SensorName = x.Sensor?.Name,
+                ReadingDate = x.ReadingDate,
+                Value = x.Value,
+            }).ToList();
+
+            return readingDto;
+        }
+
+        public async Task<ReadingDto> CreateReading(string sensorName)
+        {
+            var sensor = await _context.Sensors.FirstOrDefaultAsync(s => s.Name == sensorName);
+            if(sensor == null)
+            {
+                throw new InvalidOperationException("Sensor not found");
+            }
+
+            var random = new Random();
+            double randomValue = random.NextDouble() * 100;
+            double roundedValue = Math.Round(randomValue, 1);
+
+            var reading = new Reading
+            {
+                Id = Guid.NewGuid(),
+                Sensor = sensor,
+                ReadingDate = DateOnly.FromDateTime(DateTime.UtcNow),
+                Value = roundedValue
+            };
+
+            _context.Readings.Add(reading);
+            await _context.SaveChangesAsync();
+
+            return new ReadingDto
+            {
+                SensorName = sensor.Name,
+                ReadingDate = DateOnly.FromDateTime(DateTime.UtcNow),
+                Value = roundedValue
+            };
+        }
+    }
+}
+
+         */
+    }
+}
--- Backend/Api_Sensors/Api_Sensors/obj/Debug/net8.0/Api_Sensors.AssemblyInfo.cs
+++ Backend/Api_Sensors/Api_Sensors/obj/Debug/net8.0/Api_Sensors.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api_Sensors")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1e7b3ad5ab4997fb542627095ebb1ee6cfb68b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4197b484d534f03a68471388cf22dae5b86db506")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api_Sensors")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api_Sensors")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
