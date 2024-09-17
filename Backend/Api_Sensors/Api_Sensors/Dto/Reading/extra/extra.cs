159c51b0f0456cc6eb916798f304932c8d1367b8
Lautaro-Martin-Geredus <lautarogh789@gmail.com>
2024-09-17 16:32:52 -0300

    ajustando 2

--- Backend/Api_Sensors/Api_Sensors/Dto/Reading/extra/extra.cs
+++ Backend/Api_Sensors/Api_Sensors/Dto/Reading/extra/extra.cs
@@ -71,6 +71,115 @@ namespace Api_Sensors.Repository.Impl
     }
 }
 
+
+        sing Api_Sensors.Dto.Sensor;
+using Api_Sensors.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api_Sensors.Repository.Impl
+{
+    public class SensorRepositoryImpl : ISensorRepository
+    {
+        private readonly ApiSensoresContext _context;
+
+        public SensorRepositoryImpl(ApiSensoresContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SensorDto>> GetSensors()
+        {
+            return await _context.Sensors.
+                Select(s => new SensorDto
+                {
+                    Name = s.Name,
+                    Description = s.Description,
+                    Unit = s.Unit
+                })
+                .ToListAsync();
+        }
+
+        public async Task<SensorDto> PostSensor(SensorDto sensorDto)
+        {
+            var existingSensor = await _context.Sensors.FirstOrDefaultAsync(x => x.Name == sensorDto.Name);
+            if(existingSensor != null)
+            {
+                throw new InvalidOperationException("There is already a sensor with that name!");
+            }
+
+            var sensor = new Sensor
+            {
+                Id = Guid.NewGuid(),
+                Name = sensorDto.Name,
+                Description = sensorDto.Description,
+                Unit = sensorDto.Unit
+            };
+
+            _context.Sensors.Add(sensor);
+            await _context.SaveChangesAsync();
+
+            return new SensorDto
+            {
+                Name = sensor.Name,
+                Description = sensor.Description,
+                Unit = sensor.Unit
+            };
+        }
+
+        public async Task<SensorDto> GetSensorByName(string name)
+        {
+            var sensor = await _context.Sensors.FirstOrDefaultAsync(s => s.Name == name);
+            if(sensor == null)
+            {
+                throw new InvalidOperationException($"There is no sensor with that name: {name}!");
+            }
+
+            return new SensorDto
+            {
+                Name = sensor.Name,
+                Description = sensor.Description,
+                Unit = sensor.Unit
+            };
+        }
+
+        public async Task<SensorDto> PutSensor(string name, SensorDto sensorDto)
+        {
+            var sensor = await _context.Sensors.FirstOrDefaultAsync(s => s.Name == name);
+            if (sensor == null)
+            {
+                throw new InvalidOperationException("There is no sensor with that Name!");
+            }
+            
+            sensor.Name = sensorDto.Name;
+            sensor.Description = sensorDto.Description;
+            sensor.Unit = sensorDto.Unit;
+
+            await _context.SaveChangesAsync();
+
+            return new SensorDto
+            {
+                Name= sensor.Name,
+                Description = sensor.Description,
+                Unit = sensor.Unit
+            };
+        }
+
+        public async Task<bool> DeleteSensor(string name)
+        {
+            var sensor = await _context.Sensors.FirstOrDefaultAsync(s => s.Name == name);
+            if (sensor == null)
+            {
+                return false; 
+            }
+
+            _context.Remove(sensor);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
+
          */
     }
 }
